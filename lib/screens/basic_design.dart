97b8a8cd0b66d3ebced0fb5154f6ee035e494f86
Erick Estrada <eof105@gmail.com>
2022-10-28 14:03:41 -0500

    Diseño básico parte 3

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
 class BasicDesignScreen extends StatelessWidget {
-   
   const BasicDesignScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-        children: const [
-          Image(image: AssetImage('assets/landscape.jpeg')),
+        children: [
+          const Image(image: AssetImage('assets/landscape.jpeg')),
 
-          Title()
+          // Titulo
+          const Title(),
 
+          // button section
+          const ButtonSection(),
+
+          //Description
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: const Text('Aliquip sit reprehenderit mollit dolore voluptate id duis enim fugiat ea. Occaecat labore mollit laboris duis culpa consectetur dolore ea minim non laborum deserunt laborum labore. Esse esse ad aliqua ut eu fugiat anim aliqua laborum id quis pariatur. Duis non tempor duis fugiat sint Lorem tempor id.')
+            )
         ],
       ),
     );
@@ -30,20 +38,80 @@ class Title extends StatelessWidget {
       margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
       child: Row(
         children: [
-
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: const [
-              Text('Oeschinen Lake Campground', style: TextStyle(fontWeight: FontWeight.bold),),
-              Text('Kandersteg, Wwitzerland', style: TextStyle(color: Colors.black45),),
+              Text(
+                'Oeschinen Lake Campground',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Kandersteg, Wwitzerland',
+                style: TextStyle(color: Colors.black45),
+              ),
             ],
           ),
-          
           Expanded(child: Container()),
-
-
-          const Icon(Icons.star, color: Colors.red,),
+          const Icon(
+            Icons.star,
+            color: Colors.red,
+          ),
           const Text('41')
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const [
+          CustomButton(icon: Icons.call, text: 'CALL'),
+          CustomButton(icon: Icons.room, text: 'ROUTE'),
+          CustomButton(icon: Icons.share, text: 'SHARE'),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final Color? color;
+  final double? size;
+  const CustomButton({
+    Key? key,
+    required this.icon,
+    required this.text,
+    this.color, 
+    this.size,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: color ?? Colors.blue,
+            size: size ?? 30,
+          ),
+          Text(
+            text,
+            style: TextStyle(color: color ?? Colors.blue),
+          )
         ],
       ),
     );
